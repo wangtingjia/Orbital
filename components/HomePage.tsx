c1e6b95ffb2e4ba93eccca0c925c75fc08a3ff66
wangtingjia <wangtingjia49@gmail.com>
2022-05-24 17:20:05 +0800

    New sign in sign out

---
+++ components/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import LoginSignupScreen from './LoginSignupScreen'
+
+const HomePage = () => {
+  return (
+    <View>
+      <div>Welcome to our homepage</div>
+    </View>
+
+  )
+}
+
+export default HomePage
