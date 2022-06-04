eaff73021d247cc0b3aa6771ed754a1cba0d5e28
wangtingjia <wangtingjia49@gmail.com>
2022-06-04 13:50:49 +0800

    Add nav bar for listings page

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -3,17 +3,37 @@ import { View, Button, Text } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import LoginSignupScreen from './LoginSignupScreen'
 import { supabase } from '../lib/supabase';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+const Tab = createMaterialTopTabNavigator();
+
+
+function CreateListing({route, navigation}) {
+  return (
+    <View>
+      <Text> Start your sports activity here </Text>
+    </View>
+  )
+}
+
+function SearchListing ({route, navigation}) {
+  return (
+    <View>
+      <Text> Search for sports activity here </Text> 
+    </View>
+    
+  )
+}
 
 function Listings({navigation}){
   function checking(){
     console.log(supabase.auth.session())
   }
   return (
-    <View>
-      <Text>You will search for listings/post listings here</Text>
-    </View>
+    <Tab.Navigator> 
+      <Tab.Screen name="Initiate activity" component ={CreateListing} />
+      <Tab.Screen name="Search activities" component ={SearchListing} />
+    </Tab.Navigator>
   )
 }
 
